1a728ff1e2cd39c7739364236db99664bb5af06d
Stephen Joel <joel.stephen56.sj@gmail.com>
2021-11-12 06:21:04 +0100

    Added the chart bar file

---
+++ lib/widgets/chart.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(20),
+    );
+  }
+}
