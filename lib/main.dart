22a705407153c0db1e5ce05adf2d63b6bda438de
Stephen Joel <joel.stephen56.sj@gmail.com>
2021-12-06 19:20:54 +0100

    fixed the orientation to portrait only

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:personal_expense_tracker/widgets/chart.dart';
 import 'package:personal_expense_tracker/widgets/transaction_list.dart';
 import '../widgets/new_transaction.dart';
@@ -6,7 +7,12 @@ import '../widgets/transaction_list.dart';
 import '../models/transaction.dart';
 import './widgets/chart.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
